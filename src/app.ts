c26809307a29d68dae7aedc88003a17e2802c67f
Paramdeep Singh <psingh20@rrc.ca>
2025-10-01 17:55:27 -0500

    I setup the Express app with Morgan for logging

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+import employeeRoutes from "./api/v1/routes/employeeRoutes";
+import branchRoutes from "./api/v1/routes/branchRoutes";
+
+
+const app = express();
+
+app.use(morgan("combined"));
+app.use("/api/v1/employees", employeeRoutes);
+app.use("/api/v1/branches", branchRoutes);
+
+// Health check route
+app.get("/health", (_req: Request, res: Response) => {
+  res.status(200).send("Server is healthy");
+});
+
+export default app;
