df0a813d044f4b5c4fd62d9353dc57bca5bba834
Paramdeep Singh <psingh20@rrc.ca>
2025-10-13 19:32:19 -0500

    added the jest.setup.ts and added the serviceaccountkey in gitignore

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,9 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  moduleDirectories: ["node_modules", "src"],
-  transform: {
-    ...tsJestTransformCfg,
-     preset: "ts-jest",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
-  },
+    preset: "ts-jest",
+
+    
+    testEnvironment: "node",
+    setupFilesAfterEnv: ["<rootDir>/src/test/jest.setup.ts"],
 };
